c3981f52f61dfd4906d4b1d4ed2da102e51bdf5d
kkhan0214 <70661595+kkhan0214@users.noreply.github.com>
2025-06-18 03:58:10 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 # Colab 또는 로컬 AI 서버 주소
 ASR_SERVER_URL = "https://tops-beetle-vocal.ngrok-free.app/api/analyze-audio"
-EVAL_SERVER_URL = "https://wise-positively-octopus.ngrok-free.app/api/analyze-audio"
+EVAL_SERVER_URL = "https://4bbc-34-141-187-127.ngrok-free.app/api/analyze-audio"
